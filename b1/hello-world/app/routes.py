abee19ea1691074e1add5961faf6f1ea5fc1b955
hduc <anojn750@gmail.com>
2024-05-23 23:27:11 +0700

    using template

--- b1/hello-world/app/routes.py
+++ b1/hello-world/app/routes.py
@@ -1,6 +1,11 @@
+from flask import render_template
 from app import app
 
-@app.route ('/')
-@app.route('/index')
+@app.route('/')
 def index():
     return "Hello, World!"
+
+@app.route('/home')
+def home():
+    user={'username': 'Duc'}
+    return render_template('home.html', title='Home', user=user)
